65792acbd4ebfdea8cf16861a88c1853fc815635
Austin Johnson <technicallyaustin@gmail.com>
2021-03-02 22:27:50 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -9,4 +9,14 @@ WIN_COMBINATIONS = [
   [0,3,6], [0,4,8], [1,4,7],
   [2,5,8], [2,4,6]]
   
-  
+  def won?(board)
+  WIN_COMBINATIONS.detect do |combo|
+    win_index_1 = combo[0]
+    win_index_2 = combo[1]
+    win_index_3 = combo[2]
+
+    if (board[win_index_1] == "X" && board[win_index_2] == "X" && board[win_index_3] == "X") || (board[win_index_1] == "O" && board[win_index_2] == "O" && board[win_index_3] == "O")
+       return combo
+    end
+  end
+end
